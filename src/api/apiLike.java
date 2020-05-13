392c0111986d0c4b01049256008a9538da5225db
byend <byend@DESKTOP-MKE0A9S.lan>
2020-05-13 13:09:35 +0200

    api like

---
+++ src/api/apiLike.java
@@ -0,0 +1,48 @@
+package api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloComic;
+
+/**
+ * Servlet implementation class apiLike
+ */
+@WebServlet("/apiLike")
+public class apiLike extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public apiLike() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloComic mComic = new ModeloComic();
+		mComic.addlike(id);
+		try {
+			mComic.getConexion();
+		} catch (Exception e) {
+			
+		}
+		
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
