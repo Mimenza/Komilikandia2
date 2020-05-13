4d424ac26076022cf39dc5fc1a489ec07c3623da
byend <byend@DESKTOP-MKE0A9S.lan>
2020-05-13 13:10:16 +0200

    api delete

---
+++ src/api/apiDelete.java
@@ -0,0 +1,45 @@
+package api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloComic;
+
+/**
+ * Servlet implementation class apiDelete
+ */
+@WebServlet("/apiDelete")
+public class apiDelete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public apiDelete() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int id=Integer.parseInt(request.getParameter("id"));
+		
+		ModeloComic mComic = new ModeloComic();
+		mComic.delete(id);
+		try {
+			mComic.getConexion().close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
